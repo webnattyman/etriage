dcec0e714c2e7fb9869b7e0d24fbcb38d4ed82c7
Gustavo Guerrero <laetniagagn@gmail.com>
2016-10-04 18:40:12 -0500

    Gustavo Guerrero

--- app.js
+++ app.js
@@ -1,54 +1,31 @@
-var os = require('os');
-var static = require('node-static');
-var http = require('http');
-var socketIO = require('socket.io');
+var express = require('express');  
+var app = express();  
+var server = require('http').Server(app);  
+var io = require('socket.io')(server);
 
-var fileServer = new(static.Server)();
-var app = http.createServer(function (req, res) {
-    fileServer.serve(req, res);
-}).listen( process.env.PORT || 2013);
+var messages = [{  
+    id: 1,
+    text: "Hola soy un mensaje",
+    author: "Carlos Azaustre"
+}];
 
-var io = socketIO.listen(app);
-io.sockets.on('connection', function (socket){
-    // convenience function to log server messages on the client
-    function log(){
-        var array = [">>> Message from server:"];
-        array.push.apply(array, arguments);
-        socket.emit('log', array);
-    }
+app.use(express.static('public'));
 
-    socket.on('message', function (message) {
-            log('Client said:', message);
-    // for a real app, would be room only (not broadcast)
-            socket.broadcast.emit('message', message);
-    });
+app.get('/hello', function(req, res) {  
+  res.status(200).send("Bienvenido!");
+});
 
-    socket.on('create or join', function (room) {
-        log('Request to create or join room ' + room);
-        var listClients = io.of('/').in(room).clients;
-        var numClients = listClients.length;
-        log('Room ' + room + ' has ' + numClients + ' client(s)');
-        if (numClients == 1){
-            socket.join(room);
-            socket.emit('created', room, socket.id);
-        } else if (numClients === 2) {
-            socket.join(room);
-            socket.emit('joined', room, socket.id);
-            io.sockets.in(room).emit('ready');
-        } else { // max two clients
-            socket.emit('full', room);
-        }
-    });
+io.on('connection', function(socket) {  
+  console.log('Alguien se ha conectado con Sockets');
+  socket.emit('messages', messages);
 
-    socket.on('ipaddr', function () {
-        var ifaces = os.networkInterfaces();
-        for (var dev in ifaces) {
-            ifaces[dev].forEach(function (details) {
-                if (details.family=='IPv4' && details.address != '127.0.0.1') {
-                    socket.emit('ipaddr', details.address);
-                }
-          });
-        }
-    });
+  socket.on('new-message', function(data) {
+    messages.push(data);
 
+    io.sockets.emit('messages', messages);
+  });
+});
+
+server.listen(2013, function() {  
+  console.log("Servidor corriendo en http://localhost:2013");
 });
