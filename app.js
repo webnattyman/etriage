bf51bcec4a824a179303302e7de9827c85a33b08
Gustavo Guerrero <laetniagagn@gmail.com>
2016-10-06 17:36:43 -0500

    Gustavo Guerrero

--- app.js
+++ app.js
@@ -12,37 +12,37 @@ var messages = [{
 }];
 
 app.use(express.static('.'));
+/*app.get('/', function(req, res){
+    res.sendFile(dir_url + '/index.html');
+});*/
 
 io.on('connection', function(socket) {
     socket.join(sala);
     var socketId = io.nsps[namespace].adapter.rooms[sala].length;
     if( socketId == 1){
-        socket.join(sala);
         socket.emit('crear', sala, socket.id);
-        app.get('/', function(req, res){
-            res.sendFile(dir_url + '/index.html');
-        });
     }else if ( socketId == 2){
-        socket.join(sala);
         socket.emit('agregado', sala, socket.id);
-        app.get('/', function(req, res){
-            res.sendFile(dir_url + '/index.html');
-        });
     }else{
         console.log('Sala llena!');
-        app.get('/', function(req, res){
-            res.sendFile(dir_url + '/error.html');
-        });
     }
+    socket.emit('emit(): client ' + socket.id + ' joined room ' + sala);
+    socket.broadcast.emit('broadcast(): client ' + socket.id + ' joined room ' + sala);
     console.log('Usuarios Conectados: ' + socketId);
     //socket.emit('messages', messages);
     socket.on('new-message', function(data) {
         messages.push(data);
-        io.sockets.emit('messages', messages);
+        io.sockets.emit('new-message', messages);
     });
+    
+    socket.on('message', function (message) {
+        console.log('Got message: ', message);
+        socket.broadcast.emit('message', message);
+    });
+    
 });
 
 server.listen( process.env.PORT || 2013, function() {  
-  console.log("Servidor corriendo en local");
-  //console.log("Servidor corriendo en https://redmedix.herokuapp.com");
+  //console.log("Servidor corriendo en local");
+  console.log("Servidor corriendo en https://redmedix.herokuapp.com");
 });
