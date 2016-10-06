a7e5fa9138f01076b011ae4b341203524edc0a37
Gustavo Guerrero <laetniagagn@gmail.com>
2016-10-06 10:51:47 -0500

    Gustavo Guerrero

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var messages = [{
 
 app.use(express.static('public'));
 
-app.get('/hello', function(req, res) {  
+app.get('/', function(req, res) {  
   res.status(200).send("Bienvenido!");
 });
 
