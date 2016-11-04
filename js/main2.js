aa2a86186938d444e7ff57c94a6070d96eeec3ad
Gustavo Guerrero <laetniagagn@gmail.com>
2016-11-04 16:58:40 -0500

    work sockets, don't work videochat v.8

--- js/main2.js
+++ js/main2.js
@@ -205,7 +205,7 @@ if(roomid && roomid.length) {
     disableInputButtons();
 }
 
-var socket = io.connect('https://redmedix.herokuapp.com/', { 'forceNew': true });
+var socket = io.connect('https://redmedix.herokuapp.com:443/', { 'forceNew': true });
 
 socket.on('new-message', function(data) {  
   console.log(data);
