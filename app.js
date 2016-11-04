60fa473b4b157e75aab726eaf6f4fee3a086333f
Gustavo Guerrero <laetniagagn@gmail.com>
2016-11-04 12:18:33 -0500

    version antigua v.8

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');  
 var app = express();  
-var server = require('https').Server(app);  
+var server = require('http').Server(app);  
 var io = require('socket.io')(server);
 var sala = "chat983143145454";
 var namespace = '/';
