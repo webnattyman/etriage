62549a2c054de84c1d6f32c4faf35ab5d2223b3d
Gustavo Guerrero <laetniagagn@gmail.com>
2016-11-04 12:36:21 -0500

    version antigua v.13

--- js/main2.js
+++ js/main2.js
@@ -45,7 +45,7 @@ connection.enableLogs = true;
 
 // by default, socket.io server is assumed to be deployed on your own URL
 //connection.socketURL = '/';
-//connection.socketURL = 'https://redmedix.herokuapp.com:443/';
+connection.socketURL = 'https://redmedix.herokuapp.com:443/';
 connection.socketMessageEvent = 'Video Chat';
 
 connection.session = {
