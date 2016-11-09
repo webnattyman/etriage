d905f37137a29cc2b403d56d10bd5b20b6bae50a
Gustavo Guerrero <laetniagagn@gmail.com>
2016-11-09 17:57:18 -0500

    Cambiando user 09112016 v.19

--- js/main2.js
+++ js/main2.js
@@ -72,7 +72,7 @@ var connection = new RTCMultiConnection();
 connection.socketURL = 'https://rtcmulticonnection.herokuapp.com:443/';
 connection.socketMessageEvent = 'Video Chat';
 connection.userid = get.uid;
-connection.extra.user = array();
+connection.extra.user = [];
 //Variables de configuracion con respecto a los tipos de datos que acepta.
 connection.session = {
     video: true,
