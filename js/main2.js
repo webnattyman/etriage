e439b3dec56089348876b371cdaa47e136a4be8a
Gustavo Guerrero <laetniagagn@gmail.com>
2016-12-05 18:00:37 -0500

    Redmedix chat v-1.1.47

--- js/main2.js
+++ js/main2.js
@@ -39,10 +39,7 @@ document.getElementById('join').onclick = function() {
         rol: get.r
     };
 	var hrtotal = getDiferenciaHora(get.hc, get.tp);
-	
-	$clock.countdown(hrtotal, function(event) {
-		$(this).html(event.strftime('%H:%M:%S'));
-	});
+	initializeClock('clock', hrtotal);
     connection.sessionid = get.uid;
     connection.userid = get.uid;
     connection.rol = get.r;
@@ -100,6 +97,39 @@ function mostrarhora(){
     return hours+":"+minutes+":"+seconds+" "+dn;
 }
 
+function getTimeRemaining(endtime) {
+	var t = Date.parse(endtime) - Date.parse(new Date());
+	var seconds = Math.floor((t / 1000) % 60);
+	var minutes = Math.floor((t / 1000 / 60) % 60);
+	var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+	return {
+		'total': t,
+		'hours': hours,
+		'minutes': minutes,
+		'seconds': seconds
+	};
+}
+
+function initializeClock(id, endtime) {
+	var clock = document.getElementById(id);
+	var hoursSpan = clock.querySelector('.hours');
+	var minutesSpan = clock.querySelector('.minutes');
+	var secondsSpan = clock.querySelector('.seconds');
+
+	function updateClock() {
+		var t = getTimeRemaining(endtime);
+		hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+		minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+		secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+		if (t.total <= 0) {
+			clearInterval(timeinterval);
+		}
+	}
+
+	updateClock();
+	var timeinterval = setInterval(updateClock, 1000);
+}
 
 function getDiferenciaHora( hra_ini, duration ){
     var f       = new Date();
