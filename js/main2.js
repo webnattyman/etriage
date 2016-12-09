d705e5fefb862a2dd7301676302b2e3b75e3b1df
Gustavo Guerrero <laetniagagn@gmail.com>
2016-12-09 12:15:04 -0500

    Redmedix chat V-1.1.67

--- js/main2.js
+++ js/main2.js
@@ -330,7 +330,7 @@ function disableInputButtons() {
 
 //Funcion que abre los parametros del chat, despues de iniciar session.
 function showRoomURL(roomid) {
-    var roomQueryStringURL = "?roomid="+roomid;
+    var roomQueryStringURL = "?roomid="+roomid+"&r=1&tp="+get.tp+"&hc="+get.hc;
     var html = '<h2>Estos son los datos de tu sala:</h2><br>';
     html += 'Enlace de la sala : <a href="' + roomQueryStringURL + '" target="_blank">' + roomQueryStringURL + '</a>';
     var roomURLsDiv = document.getElementById('room-urls');
