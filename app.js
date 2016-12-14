4c5ac19a393c1c9252762e0e07e0f806c003e405
Gustavo Guerrero <laetniagagn@gmail.com>
2016-12-14 17:47:47 -0500

    Redmedix chat V-1.2.21

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use(express.static('.'));
 io.on('connection', function(socket) {
     //var socketId = io.of('/').clients().server.eio.clients.Socket.Server;
     var socketId = 1;
-	console.log(socket.client.length());
+	console.log(socket.manager.roomClients[socket.id]);
     if( socketId == 1){
         socket.emit('crear', sala, socket.id);
     }else if ( socketId == 2){
