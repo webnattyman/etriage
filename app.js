2feb0a8d2c45ecc9946ac9ebf578054afe297f00
Gustavo Guerrero <laetniagagn@gmail.com>
2016-12-19 12:37:04 -0500

    Redmedix chat V-1.2.93

--- app.js
+++ app.js
@@ -92,10 +92,7 @@ io.on('connection', function(socket) {
 		var postInsert = {id_hstcht: null, cita_hstcht: parseInt(message.cid), txt_hstcht:resp};
 		var postUpdate = {txt_hstcht:resp, cita_hstcht: parseInt(message.cid)};
 		db.query('SELECT * FROM historial_chat WHERE cita_hstcht = ?', getId, function (err, results, fields) {
-			console.log(results);
-			console.log(fields);
-			console.log(err);
-			if (results){
+			if (fields){
 				db.query('UPDATE INTO historial_chat SET txt_hstcht = ? WHERE cita_hstcht = ?', postUpdate, function (err2, results2, fields2) {});
 			}else{
 				db.query('INSERT INTO historial_chat SET ?', postInsert, function (err3, results3, fields3) {});
